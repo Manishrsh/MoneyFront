9a611c465b4626b5d9ab1ce703b577f978a9ec7c
Manishrsh <123761677+Manishrsh@users.noreply.github.com>
2024-07-09 17:47:22 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,35 @@
-import React from 'react';
+import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Moneyadd from './Componets/Moneyadd/Moneyadded'
+import Expence from './Componets/ExpenceMoney.jsx/Expence';
+import Home from './Componets/HomeCom/Home';
+
+
+import './App.css'
+import AllExpence from './Componets/ExpenceMoney.jsx/AllExpence';
+import ExpenceDatefor from './Componets/DateFormatExpenceData/ExpenceDatefor';
+import { store } from '../src/Componets/Store/store'
+import { Provider } from 'react-redux'
+import Navbars from './Componets/Navbar/Navbars';
 
 function App() {
+  
+
   return (
     <div>
+      <ThemeProvider theme={theme}>
+      <NavbarComponent />
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<div>Hi</div>} />
-          <Route path='/home' element={<div>Home</div>} />
+		      
+          <Route path='/home' element={<div>hi</div>} />
+          <Route path='/addmoney' element={<MoneyAdd/>} />
+          
         </Routes>
       </BrowserRouter>
+    </ThemeProvider>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
